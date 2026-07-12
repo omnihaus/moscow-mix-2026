52dd51672e385c4cdc6f5dea7a48982d78741a8e
Ash Sud <worldtravelash@gmail.com>
2026-07-12 22:01:14 +0400

    fix: restore page-specific production styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './context/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './views/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
